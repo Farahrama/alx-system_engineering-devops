a966787d76aa007819b42b6dc7f90d93d7b80ec2
“Farahrama” <rafarah507@gmail.com>
2024-08-14 22:15:33 +0300

    f

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""
+This module contains functions to interact with the Reddit API.
+"""
 import requests
+
 
 def top_ten(subreddit):
     """Prints the titles of the top 10 hot posts for a given subreddit."""
@@ -17,5 +21,7 @@ def top_ten(subreddit):
             print(None)
     except requests.RequestException:
         print(None)
+
+
 
 
